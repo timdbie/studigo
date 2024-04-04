3c6e287b51c2114a864e2f2757195fcfef6207f9
timdbie <timdebie67@gmail.com>
2024-04-04 10:17:18 +0200

    feat: Added basic httpclient wrapper

--- StudiGO.DAL/Infrastructure/HttpClientWrapper.cs
+++ StudiGO.DAL/Infrastructure/HttpClientWrapper.cs
@@ -1,6 +1,26 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+
 namespace StudiGO.DAL.Infrastructure;
 
 public class HttpClientWrapper
 {
-    
+    private readonly HttpClient _httpClient;
+
+    public HttpClientWrapper()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    public async Task<HttpResponseMessage> GetAsync(string requestUrl)
+    {
+        try
+        {
+            return await _httpClient.GetAsync(requestUrl);
+        }
+        catch (HttpRequestException ex)
+        {
+            throw ex;
+        }
+    }
 }
