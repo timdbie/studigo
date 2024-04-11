79d0a1bbce27dbf9f20c6fcbe37c441226759598
timdb <74897688+timdbie@users.noreply.github.com>
2024-04-11 17:49:49 +0200

    feat: Added main script back

--- StudiGO.Web/wwwroot/js/main.js
+++ StudiGO.Web/wwwroot/js/main.js
@@ -1,4 +1,11 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+$(".banner_tabs div").on("click", function() {
+    if(!$(this).hasClass("banner_tabs-selected")) {
+        $(".banner_tabs div").not(this).removeClass("banner_tabs-selected");
+        $(this).addClass("banner_tabs-selected");
+    }
+});
+
 // Write your JavaScript code.
