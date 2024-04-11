d15ef67fedb8157889e1362ca7d312910e0a335c
timdbie <timdebie67@gmail.com>
2024-04-11 14:23:04 +0200

    feat: Added stations fetching from view

--- StudiGO.Web/Pages/Index.cshtml.cs
+++ StudiGO.Web/Pages/Index.cshtml.cs
@@ -1,18 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using StudiGO.Core.Services;
 
 namespace StudiGO.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    private readonly StationsService _stationsService;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(StationsService stationsService)
     {
-        _logger = logger;
+        _stationsService = stationsService;
     }
 
-    public void OnGet()
+    public async Task OnGetAsync(string query)
     {
+        if (!string.IsNullOrEmpty(query))
+        {
+            var stationsDto = await _stationsService.GetStationsAsync(query, "NL", 10);
+            ViewData["Stations"] = stationsDto;
+        }
     }
 }
