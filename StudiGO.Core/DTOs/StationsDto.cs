fdaa9fa23b0f1acc2aff6411037cf4df1bd26092
timdbie <timdebie67@gmail.com>
2024-04-11 11:36:22 +0200

    feat: Simplified stations DTO

--- StudiGO.Core/DTOs/StationsDto.cs
+++ StudiGO.Core/DTOs/StationsDto.cs
@@ -11,17 +11,7 @@ public class Payload
     public string UICCode { get; set; }
     public int CdCode { get; set; }
     public string Code { get; set; }
-    public string IngangsDatum { get; set; }
-    public bool HeeftFaciliteiten { get; set; }
-    public bool HeeftReisassistentie { get; set; }
-    public bool HeeftVertrektijden { get; set; }
-    public string Land { get; set; }
-    public double Lat { get; set; }
-    public double Lng { get; set; }
-    public int Radius { get; set; }
-    public int NaderenRadius { get; set; }
     public Namen Namen { get; set; }
-    public List<Sporen> Sporen { get; set; }
     public string StationType { get; set; }
 }
 
@@ -30,9 +20,4 @@ public class Namen
     public string Lang { get; set; }
     public string Middel { get; set; }
     public string Kort { get; set; }
-}
-
-public class Sporen
-{
-    public string SpoorNummer { get; set; }
 }
