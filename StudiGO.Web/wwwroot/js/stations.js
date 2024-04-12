75d5695ce37405bcea5bde5ba87ba73f85ca0b89
timdb <74897688+timdbie@users.noreply.github.com>
2024-04-12 14:38:26 +0200

    feat: Made search reusable

--- StudiGO.Web/wwwroot/js/stations.js
+++ StudiGO.Web/wwwroot/js/stations.js
@@ -1,14 +1,25 @@
-function getStations(query) { 
+function getStations(query, element) { 
     $.ajax({
         url: "?handler=Stations",
         type: "GET",
         data: { query : query },
         success: function(data) {
-            console.log(data);
+            var results = element.children(".stations-search_results");
+            results.empty();
+            
+            data.payload.forEach((station) => {
+                console.log(station.namen.middel);
+                var stationDiv = $("<div>").text(station.namen.lang);
+                
+                results.append(stationDiv);
+            })
         }
     })
 }
 
-$("#button").on("click", function() {
-    getStations($("#input").val());
+$(".stations-search button").on("click", function() {
+    var search = $(this).closest(".stations-search");
+    var input = search.children("input").val();
+    
+    getStations(input, search);
 })
