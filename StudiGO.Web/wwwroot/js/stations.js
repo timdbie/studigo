69ef5ae9a169bb771815e0dbc5ddac59499b8199
timdb <74897688+timdbie@users.noreply.github.com>
2024-04-12 15:32:49 +0200

    feat: Added asynchronous handling for input

--- StudiGO.Web/wwwroot/js/stations.js
+++ StudiGO.Web/wwwroot/js/stations.js
@@ -1,25 +1,42 @@
-function getStations(query, element) { 
-    $.ajax({
-        url: "?handler=Stations",
-        type: "GET",
-        data: { query : query },
-        success: function(data) {
-            var results = element.children(".stations-search_results");
-            results.empty();
-            
-            data.payload.forEach((station) => {
-                console.log(station.namen.middel);
-                var stationDiv = $("<div>").text(station.namen.lang);
-                
-                results.append(stationDiv);
-            })
-        }
-    })
+var searchTimer;
+
+async function getStations(query) {
+    try {
+        const data = await $.ajax({
+            url: "?handler=Stations",
+            type: "GET",
+            data: { query: query }
+        });
+        return data.payload;
+    } catch (error) {
+        throw new Error("Failed to fetch stations: " + error);
+    }
 }
 
-$(".stations-search button").on("click", function() {
+async function getResults(input, element) {
+    var results = element.children(".stations-search_results");
+    results.empty();
+
+    try {
+        const stations = await getStations(input);
+        stations.forEach((station) => {
+            var stationDiv = $("<div>").text(station.namen.lang);
+            results.append(stationDiv);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+$(".stations-search input").on("input", function() {
+    clearTimeout(searchTimer);
+
+    var input = $(this).val();
     var search = $(this).closest(".stations-search");
-    var input = search.children("input").val();
     
-    getStations(input, search);
-})
+    searchTimer = setTimeout(async function() {
+        if(input !== "") {
+            await getResults(input, search);
+        }
+    }, 500);
+});
