1150c0bde18c0407a93fc987cc4c9037fd85cd37
timdbie <timdebie67@gmail.com>
2024-04-15 16:40:20 +0200

    feat: Added trips service

---
+++ StudiGO.Core/Services/TripsService.cs
@@ -0,0 +1,21 @@
+﻿using StudiGO.Core.Interfaces;
+using StudiGO.Core.DTOs;
+
+namespace StudiGO.Core.Services;
+
+public class TripsService
+{
+    private readonly ITripsRepository _tripsRepository;
+
+    public TripsService(ITripsRepository tripsRepository)
+    {
+        _tripsRepository = tripsRepository;
+    }
+    
+    public async Task<TripsDto> GetTripsAsync(string from, string to, string datetime)
+    {
+        var tripsDto = await _tripsRepository.GetTripsAsync(from, to, datetime);
+        
+        return tripsDto;
+    }
+}
