349bf12d45dccf81d853860cf05a5e3b0b2cbb3e
timdbie <timdebie67@gmail.com>
2024-04-15 14:28:37 +0200

    feat: Added repository for trips

---
+++ StudiGO.DAL/Repositories/TripsRepository.cs
@@ -0,0 +1,13 @@
+﻿using StudiGO.Core.DTOs;
+using StudiGO.Core.Interfaces;
+
+namespace StudiGO.DAL.Repositories;
+
+public class TripsRepository : ApiBaseRepository
+{
+    public async Task<TripsDto> GetTripsAsync(string fromStation, string toStation, DateTime dateTime)
+    {
+        string endpoint = $"/reisinformatie-api/api/v3/trips?fromStation={fromStation}&toStation={toStation}&dateTime={dateTime}";
+        return await GetApiResponseAsync<TripsDto>(endpoint);
+    }
+}
