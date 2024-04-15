4b46bc4c2d90116811e0bd84a2753e75ae72f2c5
timdbie <timdebie67@gmail.com>
2024-04-15 16:34:38 +0200

    feat: Added datetime inputs that submit as one

---
+++ StudiGO.Web/wwwroot/js/trips.js
@@ -0,0 +1,9 @@
+﻿$(".planner").submit(function(event) {
+    var dateVal = $(".planner_date input").val();
+    var timeVal = $(".planner_time input").val();
+    var datetime = dateVal + "T" + timeVal;
+    
+    $("#datetime").val(datetime);
+    
+    return true;
+})
