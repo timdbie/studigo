5c02db82a7cdd91f5be45e2df8bfc9ecfb3df1c0
timdbie <timdebie67@gmail.com>
2024-04-15 18:06:23 +0200

    feat: Made getting trips async

--- StudiGO.Web/wwwroot/js/trips.js
+++ StudiGO.Web/wwwroot/js/trips.js
@@ -1,9 +1,33 @@
-﻿$(".planner").submit(function(event) {
-    var dateVal = $(".planner_date input").val();
-    var timeVal = $(".planner_time input").val();
-    var datetime = dateVal + "T" + timeVal;
-    
-    $("#datetime").val(datetime);
-    
-    return true;
-})
+﻿async function getTrips(formData) {
+    try {
+        const response = await $.ajax({
+            url: "?handler=Trips",
+            type: "GET",
+            data: formData
+        });
+        console.log(response);
+    } catch (error) {
+        console.error("Failed to fetch trips:", error);
+    }
+}
+
+$(".planner").submit(async function(event) {
+    event.preventDefault();
+
+    var from = $(".planner_search input[name='from']").val();
+    var to = $(".planner_search input[name='to']").val();
+
+    var date = $(".planner_date input").val();
+    var time = $(".planner_time input").val();
+    var datetime = date + "T" + time;
+
+    var formData = {
+        fromStation: from,
+        toStation: to,
+        datetime: datetime
+    }
+
+    await getTrips(formData);
+
+    return false;
+});
