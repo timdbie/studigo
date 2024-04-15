285d3b25974fa176fccdf9fc0136f6c64bf76333
timdbie <timdebie67@gmail.com>
2024-04-15 15:06:18 +0200

    feat: Added DTO and Interface for trips

---
+++ StudiGO.Core/DTOs/TripsDto.cs
@@ -0,0 +1,32 @@
+﻿namespace StudiGO.Core.DTOs;
+
+public class TripsDto
+{
+    public List<Trip> trips { get; set;}
+}
+
+public class Trip
+{
+    public int plannedDurationInMinutes { get; set; }
+    public int actualDurationInMinutes { get; set; }
+    public int transfers { get; set; }
+    public List<Leg> legs { get; set; }
+}
+
+public class Leg
+{
+    public Origin origin { get; set; }
+    public Destination destination { get; set; }
+}
+
+public class Origin
+{
+    public DateTime plannedDateTime { get; set; }
+    public DateTime actualDateTime { get; set; }
+}
+
+public class Destination
+{
+    public DateTime plannedDateTime { get; set; }
+    public DateTime actualDateTime { get; set; }
+}
--- StudiGO.DAL/Repositories/TripsRepository.cs
+++ StudiGO.DAL/Repositories/TripsRepository.cs
@@ -5,7 +5,7 @@ namespace StudiGO.DAL.Repositories;
 
 public class TripsRepository : ApiBaseRepository
 {
-    public async Task<TripsDto> GetTripsAsync(string fromStation, string toStation, DateTime dateTime)
+    public async Task<TripsDto> GetTripsAsync(string fromStation, string toStation, string dateTime)
     {
         string endpoint = $"/reisinformatie-api/api/v3/trips?fromStation={fromStation}&toStation={toStation}&dateTime={dateTime}";
         return await GetApiResponseAsync<TripsDto>(endpoint);
