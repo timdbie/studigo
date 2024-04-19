7b2b38da806dba84df36e054ea4aaf67fdf2e056
timdbie <timdebie67@gmail.com>
2024-04-19 14:41:48 +0200

    feat: Updated DTO for extra information

--- StudiGO.Core/DTOs/TripsDto.cs
+++ StudiGO.Core/DTOs/TripsDto.cs
@@ -18,16 +18,39 @@ public class Leg
 {
     public Origin Origin { get; set; }
     public Destination Destination { get; set; }
+    public Product Product { get; set; }
 }
 
 public class Origin
 {
+    public string Name { get; set; }
     public DateTime PlannedDateTime { get; set; }
     public DateTime ActualDateTime { get; set; }
+    public string PlannedTrack { get; set; }
+    public string ActualTrack { get; set; }
 }
 
 public class Destination
 {
+    public string Name { get; set; }
     public DateTime PlannedDateTime { get; set; }
     public DateTime ActualDateTime { get; set; }
+    public string PlannedTrack { get; set; }
+    public string ActualTrack { get; set; }
+}
+
+public class Product
+{
+    public List<Note> Notes { get; set; }
+    public List<TransferMessage> TransferMessages { get; set; }
+}
+
+public class Note
+{
+    public string Value { get; set; }
+}
+
+public class TransferMessage
+{
+    public string Message { get; set; }
 }
