99e50b2c12b81a6f5762b1cd06724fa7345de393
timdbie <timdebie67@gmail.com>
2024-04-22 17:51:44 +0200

    feat: Implemented a viewmodel for trip results

---
+++ StudiGO.Web/Models/TripViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace StudiGO.Models;
+
+public class TripViewModel
+{
+    public string PlannedDuration { get; set; }
+    public string ActualDuration { get; set; }
+    public string PlannedDepartureTime { get; set; }
+    public string PlannedArrivalTime { get; set; }
+    public int Transfers { get; set; }
+}
--- StudiGO.Web/Pages/Index.cshtml.cs
+++ StudiGO.Web/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using StudiGO.Core.Services;
 using StudiGO.Core.DTOs;
+using StudiGO.Models;
 
 namespace StudiGO.Pages;
 
@@ -15,17 +16,42 @@ public class IndexModel : PageModel
         _stationsService = stationsService;
         _tripsService = tripsService;
     }
-    
-    public List<Trip> Trips { get; private set; }
-    
+
+    private List<Trip> _trips { get; set; }
+    public List<TripViewModel> Trips { get; private set; }
+
     public async Task<IActionResult> OnGetTripsAsync(string fromStation, string toStation, string date, string time)
     {
         string dateTime = date + "T" + time;
+        
         TripsDto tripsDto = await _tripsService.GetTripsAsync(fromStation, toStation, dateTime);
-        Trips = tripsDto.trips;
-    
+        _trips = tripsDto.trips;
+
+        Trips = new List<TripViewModel>();
+
+        foreach (var trip in _trips)
+        {
+            string plannedDuration = @TimeSpan.FromMinutes(trip.PlannedDurationInMinutes).ToString(@"hh\:mm");
+            string actualDuration = @TimeSpan.FromMinutes(trip.ActualDurationInMinutes).ToString(@"hh\:mm");
+            string plannedDepartureTime = trip.Legs[0].Origin.PlannedDateTime.ToString("HH:mm");
+            string plannedArrivalTime = trip.Legs[^1].Destination.PlannedDateTime.ToString("HH:mm");
+            int transfers = trip.Transfers;
+
+            var tripViewModel = new TripViewModel
+            {
+                PlannedDuration = plannedDuration,
+                ActualDuration = actualDuration,
+                PlannedDepartureTime = plannedDepartureTime,
+                PlannedArrivalTime = plannedArrivalTime,
+                Transfers =  transfers,
+            };
+            
+            Trips.Add(tripViewModel);
+        }
+
         return Page();
     }
+    
     
     public async Task<IActionResult> OnGetStationsAsync(string query)
     {
