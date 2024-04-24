5bf095efbbf787fe9e2b9de734d269bd27d3d330
timdbie <timdebie67@gmail.com>
2024-04-24 16:25:38 +0200

    feat: Added function logic for fetching trip and details

--- StudiGO.Web/wwwroot/js/trips.js
+++ StudiGO.Web/wwwroot/js/trips.js
@@ -3,14 +3,31 @@
    
    var formData = $(this).serialize();
    getTrips(formData);
-   
-   window.location.href = window.location.origin + "#/?" + formData;
 });
 
 async function getTrips(formData) {
-    const data = await $.ajax({
+    const trips = await $.ajax({
         url: `?handler=Trips&${formData}`,
         type: "GET",
     });
-    console.log(data);
+    console.log(trips);
+    
+    var context = trips[1].context;
+    var tripDetails = await getTripDetails(context);
+    
+    window.location.href = window.location.origin + "#/?" + formData + "&context=" + context
 }
+
+async function getTripDetails(context) {
+    const tripDetails = await $.ajax({
+        url: `?handler=TripDetails`,
+        type: "GET",
+        data: { context: context }
+    })
+    console.log(tripDetails)
+    return tripDetails;
+}
+
+$(window).on('hashchange', function(e){
+    
+});
