88d1439911c599cca8f6ea1719e8cdecfe2183b4
timdbie <timdebie67@gmail.com>
2024-04-25 15:03:58 +0200

    feat: Implemented on change for each parameter

--- StudiGO.Web/wwwroot/js/trips.js
+++ StudiGO.Web/wwwroot/js/trips.js
@@ -1,4 +1,44 @@
-﻿async function fetchTrips(fromStation, toStation, dateTime) {
+﻿var prevFromStation, prevToStation, prevDateTime, prevContext;
+
+async function updateContent() {
+    var hashParams = window.location.hash.substring(2);
+    var params = new URLSearchParams(hashParams);
+
+    var fromStation = params.get('fromStation');
+    var toStation = params.get('toStation');
+    var dateTime = params.get('dateTime');
+    var context = params.get('context');
+
+    var tripsParamsChanged = fromStation !== prevFromStation || toStation !== prevToStation || dateTime !== prevDateTime;
+    var contextParamsChanged = context !== prevContext;
+
+    prevFromStation = fromStation;
+    prevToStation = toStation;
+    prevDateTime = dateTime;
+    prevContext = context;
+
+    if (fromStation && toStation && dateTime) {
+        if (tripsParamsChanged) {
+            var trips = await fetchTrips(fromStation, toStation, dateTime)
+            var tripsParam = params;
+
+            if (context) {
+                tripsParam.delete("context")
+            }
+            
+            createTrips(trips, tripsParam);
+        }
+    }
+
+    if (context) {
+        if (contextParamsChanged) {
+            var tripDetails = await fetchTripDetails(context);
+            createTripDetails(tripDetails);
+        }
+    }
+}
+
+async function fetchTrips(fromStation, toStation, dateTime) {
     const trips = await $.ajax({
         url: "?handler=Trips",
         type: "GET",
@@ -20,32 +60,6 @@ async function fetchTripDetails(context) {
     })
     console.log(tripDetails);
     return tripDetails;
-}
-
-async function updateContent() {
-    var hashParams = window.location.hash.substring(2);
-    var params = new URLSearchParams(hashParams);
-    
-    var fromStation = params.get('fromStation');
-    var toStation = params.get('toStation');
-    var dateTime = params.get('dateTime');
-    var context = params.get('context');
-    
-    if (fromStation && toStation && dateTime) {
-        var trips = await fetchTrips(fromStation, toStation, dateTime)
-        var tripsParam = params;
-        
-        if(context) {
-            tripsParam.delete("context")
-        }
-        
-        createTrips(trips, tripsParam);
-    }
-    
-    if(context) {
-        var tripDetails = await fetchTripDetails(context);
-        createTripDetails(tripDetails);
-    }
 }
 
 updateContent();
