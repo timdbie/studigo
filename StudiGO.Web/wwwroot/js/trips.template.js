f61bb1e173e3b73ae5950b2df7d941a96ef8c2c3
timdbie <timdebie67@gmail.com>
2024-04-26 17:38:52 +0200

    feat: Changed scaling of results

--- StudiGO.Web/wwwroot/js/trips.template.js
+++ StudiGO.Web/wwwroot/js/trips.template.js
@@ -16,8 +16,14 @@ function createTripResult(trip, tripsParam) {
                         </svg>
                         <span class="trips_timespan">${trip.plannedArrivalTime}</span>
                     </div>
-                    <div class="trip_info">
+                    <div class="trips_info">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                            <path fill="currentColor" fill-rule="evenodd" d="M12 2.75a9.25 9.25 0 1 0 0 18.5a9.25 9.25 0 0 0 0-18.5M1.25 12C1.25 6.063 6.063 1.25 12 1.25S22.75 6.063 22.75 12S17.937 22.75 12 22.75S1.25 17.937 1.25 12M12 7.25a.75.75 0 0 1 .75.75v3.69l2.28 2.28a.75.75 0 1 1-1.06 1.06l-2.5-2.5a.75.75 0 0 1-.22-.53V8a.75.75 0 0 1 .75-.75" clip-rule="evenodd" />
+                        </svg>
                         <span>${trip.plannedDuration}</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                            <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2 12h2m16-5h2M4 12a2 2 0 1 0 4 0a2 2 0 1 0-4 0m12-5a2 2 0 1 0 4 0a2 2 0 1 0-4 0m4 10h2m-6 0a2 2 0 1 0 4 0a2 2 0 1 0-4 0m-8.5-6.5L16 7" />
+                        </svg>
                         <span>${trip.transfers}</span>
                     </div>
                 </div>
