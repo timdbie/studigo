b85b452b1b6d58110d86416ec1b6df60ab657da0
timdbie <timdebie67@gmail.com>
2024-04-29 16:40:14 +0200

    feat: Added delay to trip results

--- StudiGO.Web/Models/TripViewModel.cs
+++ StudiGO.Web/Models/TripViewModel.cs
@@ -8,8 +8,10 @@ namespace StudiGO.Models
         public string Context { get; set; }
         public string PlannedDuration { get; set; }
         public string ActualDuration { get; set; }
-        public string PlannedDepartureTime { get; set; }
-        public string PlannedArrivalTime { get; set; }
+        public string DepartureTime { get; set; }
+        public double DepartureDelay { get; set; }
+        public string ArrivalTime { get; set; }
+        public double ArrivalDelay { get; set; }
         public int Transfers { get; set; }
 
         public static TripViewModel FromDto(Trip trip)
@@ -19,8 +21,10 @@ namespace StudiGO.Models
                 Context = Uri.EscapeDataString(trip.CtxRecon),
                 PlannedDuration = TimeSpan.FromMinutes(trip.PlannedDurationInMinutes).ToString("hh\\:mm"),
                 ActualDuration = TimeSpan.FromMinutes(trip.ActualDurationInMinutes).ToString("hh\\:mm"),
-                PlannedDepartureTime = trip.Legs[0].Origin.PlannedDateTime.ToString("HH:mm"),
-                PlannedArrivalTime = trip.Legs[^1].Destination.PlannedDateTime.ToString("HH:mm"),
+                DepartureTime = trip.Legs[0].Origin.PlannedDateTime.ToString("HH:mm"),
+                DepartureDelay = trip.Legs[0].Origin.ActualDateTime == DateTime.MinValue ? 0 : (trip.Legs[0].Origin.ActualDateTime - trip.Legs[0].Origin.PlannedDateTime).TotalMinutes,
+                ArrivalTime = trip.Legs[^1].Destination.PlannedDateTime.ToString("HH:mm"),
+                ArrivalDelay = trip.Legs[^1].Destination.ActualDateTime == DateTime.MinValue ? 0 : (trip.Legs[^1].Destination.ActualDateTime - trip.Legs[^1].Destination.PlannedDateTime).TotalMinutes,
                 Transfers = trip.Transfers,
             };
                 
