6b1df1b45a8461bce57b1063f92052c0b8783f2d
timdbie <timdebie67@gmail.com>
2024-04-30 12:50:52 +0200

    feat: Added extra duration color

--- StudiGO.Web/Models/TripViewModel.cs
+++ StudiGO.Web/Models/TripViewModel.cs
@@ -8,6 +8,7 @@ namespace StudiGO.Models
         public string Context { get; set; }
         public string PlannedDuration { get; set; }
         public string ActualDuration { get; set; }
+        public bool DurationLongerThanPlanned { get; set; }
         public string DepartureTime { get; set; }
         public double DepartureDelay { get; set; }
         public string ArrivalTime { get; set; }
@@ -21,6 +22,7 @@ namespace StudiGO.Models
                 Context = Uri.EscapeDataString(trip.CtxRecon),
                 PlannedDuration = TimeSpan.FromMinutes(trip.PlannedDurationInMinutes).ToString("h\\:mm"),
                 ActualDuration = TimeSpan.FromMinutes(trip.ActualDurationInMinutes).ToString("h\\:mm"),
+                DurationLongerThanPlanned = trip.ActualDurationInMinutes > trip.PlannedDurationInMinutes,
                 DepartureTime = trip.Legs[0].Origin.PlannedDateTime.ToString("HH:mm"),
                 DepartureDelay = trip.Legs[0].Origin.ActualDateTime == DateTime.MinValue ? 0 : (trip.Legs[0].Origin.ActualDateTime - trip.Legs[0].Origin.PlannedDateTime).TotalMinutes,
                 ArrivalTime = trip.Legs[^1].Destination.PlannedDateTime.ToString("HH:mm"),
