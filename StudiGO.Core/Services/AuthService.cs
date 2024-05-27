5b12b3f77c057f0258ad497a614b8a40c0f421b8
timdbie <timdebie67@gmail.com>
2024-05-27 17:24:02 +0200

    feat: Added service

---
+++ StudiGO.Core/Services/AuthService.cs
@@ -0,0 +1,13 @@
+﻿using StudiGO.Core.Interfaces;
+
+namespace StudiGO.Core.Services;
+
+public class AuthService
+{
+    private readonly IMySqlRepository _mySqlRepository;
+
+    public AuthService(IMySqlRepository mySqlRepository)
+    {
+        _mySqlRepository = mySqlRepository;
+    }
+}
